9a0b5ae371fa01572a27d6d1a991a3aee7c9dbe0
kovacs_s <sebastien.kovacs@epitech.eu>
2014-11-12 16:20:30 +0900

    Parse V0.1

---
+++ vm/include/parser/Lexer.hpp
@@ -0,0 +1,48 @@
+#ifndef __BRICKSVM_PARSER_LEXER_HPP__
+# define __BRICKSVM_PARSER_LEXER_HPP__
+
+# include <memory>
+# include <map>
+# include <functional>
+
+# include "interpreter/Instruction.hpp"
+# include "interpreter/ValueParameter.hpp"
+# include "interpreter/InstructionParameter.hpp"
+# include "interpreter/Value.hpp"
+# include "core/Utils.hpp"
+
+namespace bricksvm
+{
+	namespace parser
+	{
+		class Lexer
+		{
+		private:
+			unsigned int			_position;
+			int						_lineNumber;
+			int						_parentheses;
+			std::string				_line;
+			std::map<std::string, interpreter::Type> _stringTypes;
+			std::map<std::string, int> _labels;
+
+		public:
+			Lexer();
+			~Lexer();
+
+		public:
+			std::shared_ptr<interpreter::Instruction> getNextInstruction();
+			void addNewLine(std::string const &line, int lineNumber);
+
+		private:
+			std::shared_ptr<interpreter::AParameter> getNextParameter();
+			interpreter::AParameter::Type getParameterType();
+			interpreter::Value const &convertStringToValue(std::string val, interpreter::Type type);
+			bool hasParameterIdentifier();
+			interpreter::Value const &Lexer::getLabelLine(std::string val);
+			void functionCall(std::shared_ptr<interpreter::Instruction> &instruction);
+
+		};
+	}
+}
+
+#endif
--- vm/src/parser/Parser.cpp
+++ vm/src/parser/Parser.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include "parser/Parser.hpp"
+#include "parser/Lexer.hpp"
 #include "exception/FileNotFoundException.hpp"
 
 
@@ -16,17 +17,33 @@ namespace bricksvm
 
 		std::shared_ptr<interpreter::Program> Parser::generateProgramFromFile(std::string const &fileName)
 		{
-			std::shared_ptr<interpreter::Program>	newProgram;
+			std::shared_ptr<interpreter::Program>	newProgram(new interpreter::Program());
 			std::ifstream source;
 			std::string line;
+			int lineNumber = 1;
 
-			source.open(fileName);
-			if (source.good())
-				throw exception::FileNotFoundException("Cannot find programe file:" + fileName);
-			while (std::getline(source, line))
+			/*source.open(fileName.c_str());
+			std::cerr << source.fail() << std::endl;
+			if (!source.good())
+				throw exception::FileNotFoundException("Cannot find programe file:" + fileName);*/
+			std::list<std::string> program;
+			program.push_back("function1(1)");
+			program.push_back("label:");
+			program.push_back("function2(1, function3(2))");
+			program.push_back("jump(label)");
+			parser::Lexer lexer;
+			for (std::list<std::string>::iterator it = program.begin(); it != program.end(); it++)
 			{
-				
+			/*while (std::getline(source, line))
+			{*/
+				lexer.addNewLine(*it, lineNumber);
+				std::shared_ptr<interpreter::Instruction> instr = lexer.getNextInstruction();
+				if (instr->getName() != "")
+					newProgram->addInstruction(instr);
+				++lineNumber;
 			}
+			//}
+			newProgram->dumpProgram();
 
 			source.close();
 
